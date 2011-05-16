a37748759b527c8f00f8c43bf34f5335af2c33e0
Akira Matsuda <ronnie@dio.jp>
2011-05-16 17:20:04 +0900

    railtie

--- lib/html5_validators.rb
+++ lib/html5_validators.rb
@@ -1,3 +1,16 @@
+require 'rails'
+
 module Html5Validators
-  # Your code goes here...
+  class Railtie < ::Rails::Railtie #:nodoc:
+    initializer 'html5_validators' do |app|
+      ActiveSupport.on_load(:active_record) do
+        require File.join(File.dirname(__FILE__), 'html5_validators/active_model/helper_methods')
+        # see: https://github.com/rails/rails/pull/1085
+        require File.join(File.dirname(__FILE__), 'html5_validators/active_model/initializer_monkey_patches')
+      end
+      ActiveSupport.on_load(:action_view) do
+        require File.join(File.dirname(__FILE__), 'html5_validators/action_view/form_helpers')
+      end
+    end
+  end
 end
