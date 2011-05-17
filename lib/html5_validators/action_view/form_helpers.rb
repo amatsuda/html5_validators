89fb05022edee7074eb0bb629a3f789ad768a4e6
Akira Matsuda <ronnie@dio.jp>
2011-05-17 11:13:18 +0900

    add required validator to text_area, radio_button, and check_box

--- lib/html5_validators/action_view/form_helpers.rb
+++ lib/html5_validators/action_view/form_helpers.rb
@@ -10,8 +10,31 @@ module ActionView
         end
         to_input_field_tag_without_html5_attributes field_type, options
       end
-
       alias_method_chain :to_input_field_tag, :html5_attributes
+
+      def to_text_area_tag_with_html5_attributes(options = {})
+        if object.class.ancestors.include?(ActiveModel::Validations)
+          options["required"] ||= object.class.attribute_required?(method_name)
+        end
+        to_text_area_tag_without_html5_attributes options
+      end
+      alias_method_chain :to_text_area_tag, :html5_attributes
+
+      def to_radio_button_tag_with_html5_attributes(tag_value, options = {})
+        if object.class.ancestors.include?(ActiveModel::Validations)
+          options["required"] ||= object.class.attribute_required?(method_name)
+        end
+        to_radio_button_tag_without_html5_attributes tag_value, options
+      end
+      alias_method_chain :to_radio_button_tag, :html5_attributes
+
+      def to_check_box_tag_with_html5_attributes(options = {}, checked_value = "1", unchecked_value = "0")
+        if object.class.ancestors.include?(ActiveModel::Validations)
+          options["required"] ||= object.class.attribute_required?(method_name)
+        end
+        to_check_box_tag_without_html5_attributes options, checked_value, unchecked_value
+      end
+      alias_method_chain :to_check_box_tag, :html5_attributes
     end
   end
 end
