77d484e4d390a9dd2ddbc3a209f6650964b01c03
Akira Matsuda <ronnie@dio.jp>
2012-05-13 11:23:55 +0900

    simplify require

--- lib/html5_validators.rb
+++ lib/html5_validators.rb
@@ -4,13 +4,13 @@ module Html5Validators
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'html5_validators' do |app|
       ActiveSupport.on_load(:active_record) do
-        require File.join(File.dirname(__FILE__), 'html5_validators/active_model/helper_methods')
+        require 'html5_validators/active_model/helper_methods'
         if (Rails.version < '3.1.0.beta2') && (Rails.version != '3.1.0')
-          require File.join(File.dirname(__FILE__), 'html5_validators/active_model/initializer_monkey_patches')
+          require 'html5_validators/active_model/initializer_monkey_patches'
         end
       end
       ActiveSupport.on_load(:action_view) do
-        require File.join(File.dirname(__FILE__), 'html5_validators/action_view/form_helpers')
+        require 'html5_validators/action_view/form_helpers'
       end
     end
   end
