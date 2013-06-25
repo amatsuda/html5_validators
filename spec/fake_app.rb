dc4ed23433580c2427f9fbe75b06e1ee7ba66d97
Akira Matsuda <ronnie@dio.jp>
2013-06-26 03:26:01 +0900

    [rails4] "config.eager_load is set to nil. Please update your config/environments/*.rb files accordingly:"

--- spec/fake_app.rb
+++ spec/fake_app.rb
@@ -8,6 +8,7 @@ app = Class.new(Rails::Application)
 app.config.secret_token = "You know I'm born to lose, and gambling's for fools, But that's the way I like it baby, I don't wanna live for ever, And don't forget the joker!"
 app.config.session_store :cookie_store, :key => '_myapp_session'
 app.config.active_support.deprecation = :log
+app.config.eager_load = false
 app.initialize!
 
 # routes
