e2eee4ce34f9f83c54fd626234d0c30001f435fa
Akira Matsuda <ronnie@dio.jp>
2018-08-01 15:23:36 +0900

    AR 4 has no default_validation_context definition
    and refined method doesn't respond to respond_to?

--- lib/html5_validators/active_model/helper_methods.rb
+++ lib/html5_validators/active_model/helper_methods.rb
@@ -1,12 +1,31 @@
 # frozen_string_literal: true
 
+module Html5Validators
+  module DafaultValidationContext
+    if defined?(::ActiveRecord::Base) && (::ActiveRecord::VERSION::MAJOR < 5)
+      refine ::ActiveRecord::Base do
+        def default_validation_context
+          new_record? ? :create : :update
+        end
+      end
+    end
+    refine ::ActiveModel::Validations::HelperMethods do
+      def default_validation_context
+        nil
+      end
+    end
+  end
+end
+
 module ActiveModel
   module Validations
     module HelperMethods
+      using Html5Validators::DafaultValidationContext
+
       def attribute_required?(attribute)
         self.class.validators.grep(PresenceValidator).any? do |v|
           if v.attributes.include?(attribute.to_sym) && (v.options.keys & [:if, :unless]).empty?
-            if (on = v.options[:on]) && respond_to?(:default_validation_context, true)
+            if (on = v.options[:on])
               Array(on).include? default_validation_context
             else
               true
@@ -18,7 +37,7 @@ module ActiveModel
       def attribute_maxlength(attribute)
         self.class.validators.grep(LengthValidator).select {|v|
           if v.attributes.include?(attribute.to_sym) && (v.options.keys & [:maximum, :is]).any? && (v.options.keys & [:if, :unless, :tokenizer]).empty?
-            if (on = v.options[:on]) && respond_to?(:default_validation_context, true)
+            if (on = v.options[:on])
               v if Array(on).include? default_validation_context
             else
               v
@@ -30,7 +49,7 @@ module ActiveModel
       def attribute_minlength(attribute)
         self.class.validators.grep(LengthValidator).select {|v|
           if v.attributes.include?(attribute.to_sym) && (v.options.keys & [:minimum, :is]).any? && (v.options.keys & [:if, :unless, :allow_nil, :allow_blank, :tokenizer]).empty?
-            if (on = v.options[:on]) && respond_to?(:default_validation_context, true)
+            if (on = v.options[:on])
               v if Array(on).include? default_validation_context
             else
               v
@@ -42,7 +61,7 @@ module ActiveModel
       def attribute_max(attribute)
         self.class.validators.grep(NumericalityValidator).select {|v|
           if v.attributes.include?(attribute.to_sym) && (v.options.keys & [:less_than, :less_than_or_equal_to]).any? && (v.options.keys & [:if, :unless, :allow_nil, :allow_blank]).empty?
-            if (on = v.options[:on]) && respond_to?(:default_validation_context, true)
+            if (on = v.options[:on])
               v if Array(on).include? default_validation_context
             else
               v
@@ -54,7 +73,7 @@ module ActiveModel
       def attribute_min(attribute)
         self.class.validators.grep(NumericalityValidator).select {|v|
           if v.attributes.include?(attribute.to_sym) && (v.options.keys & [:greater_than, :greater_than_or_equal_to]).any? && (v.options.keys & [:if, :unless, :allow_nil, :allow_blank]).empty?
-            if (on = v.options[:on]) && respond_to?(:default_validation_context, true)
+            if (on = v.options[:on])
               v if Array(on).include? default_validation_context
             else
               v
